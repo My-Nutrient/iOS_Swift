7c69595c6bd480d80a5c5238e0370744813d82f0
ParkJunYoung <j77777y@naver.com>
2024-09-12 20:35:46 +0900

    .

---
+++ MynutrientPT/MynutrientPT/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  MynutrientPT
+//
+//  Created by 박준영 on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("1")
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text("1")
+                }
+            Text("2")
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text("2")
+                }
+            Text("3")
+                .tabItem {
+                    Image(systemName: "3.circle")
+                    Text("3")
+                }
+        }
+        .font(.largeTitle)
+    }
+}
+
+#Preview {
+    ContentView()
+}
