3874fc6a452ac7230e66ba62877571cfc60a9e6a
ParkJunYoung <j77777y@naver.com>
2024-09-19 20:37:41 +0900

    [add]

---
+++ MynutrientPT/MynutrientPT/MynutrientPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MynutrientPTApp.swift
+//  MynutrientPT
+//
+//  Created by 박준영 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MynutrientPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
